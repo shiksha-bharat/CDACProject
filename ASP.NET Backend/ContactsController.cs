bd1751ffa96043a2900b3e8b55241f43e7fe2081
prakhart07 <138373549+prakhart07@users.noreply.github.com>
2023-10-25 01:33:54 +0530

    Add files via upload

---
+++ ASP.NET Backend/ContactsController.cs
@@ -0,0 +1,45 @@
+﻿using CEDBMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CEDBMS.Controllers
+{
+    public class ContactsController : ApiController
+    {
+        
+        public HttpResponseMessage Get()
+        {
+            List<contactu> details = new List<contactu>();
+            using (shikhshabharatEntities dbContext = new shikhshabharatEntities())
+            {
+                details = dbContext.contactus.ToList();
+                if (details.Count == 0)
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, "Please try again later ");
+                }
+                return Request.CreateResponse(HttpStatusCode.OK, details);
+            }
+        } 
+
+        public HttpResponseMessage Post(contactu details) 
+        {
+            using(shikhshabharatEntities dbContext=new shikhshabharatEntities())
+            {
+                if(details != null)
+                {
+                    dbContext.contactus.Add(details);
+                    dbContext.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK, details);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Please provide required information");
+                }
+            }    
+        }
+    }
+}
